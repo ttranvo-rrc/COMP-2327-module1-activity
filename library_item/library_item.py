29ff75fa36bcda23b94cc7e3b8e29131c39d3ccf
Tri Tranvo <ttranvo@academic.rrc.ca>
2025-09-05 12:30:53 -0500

    Added Validation and error raising for LibraryItem class attributes.

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -1,4 +1,3 @@
-
 __author__ = "Tri Tranvo"
 __version__ = "1.0.0"
 
@@ -12,5 +11,21 @@ class LibraryItem:
     def __init__(self, title: str, author: str, genre: Genre):
         """
         """
+
+        if len(title.strip()) > 0:
+            self.__title = title
+        else:
+            raise ValueError("Title cannot be blank.")
+        
+        if len(author.strip()) > 0:
+            self.__author = author
+        else:
+            raise ValueError("Author cannot be blank.")
+        
+        if isinstance(genre,Genre):
+            self.__genre = genre
+        else:
+            raise ValueError("Invalid Genre.")
+    
 
 
