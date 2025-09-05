fa472c5b359a8ff619dc78e71126a602e59694bc
Tri Tranvo <ttranvo@academic.rrc.ca>
2025-09-05 13:55:55 -0500

    Added coding statements as instructed by module comments.

--- activity_01_main.py
+++ activity_01_main.py
@@ -2,10 +2,12 @@
 Description: A client program written to verify correctness of 
 the activity classes.
 """
-__author__ = "ACE Faculty"
+__author__ = "ACE Faculty, Tri Tranvo"
 __version__ = "1.0.0"
 __credits__ = ""
 
+from genre.genre import Genre
+from library_item.library_item import LibraryItem
 
 def main():
     """Test the functionality of the methods encapsulated 
@@ -18,14 +20,24 @@ def main():
     # 1. Code a statement which creates an instance of the LibraryItem class with valid inputs.
     # Use your own unique valid values for the inputs to the class.
 
-
+    try:
+        book = LibraryItem("Hobbit", "Tolkien", Genre.FANTASY)
+    except ValueError as e:
+        print(e)
+         
     # 2. Using the instance defined above, and the class Accessors, print 
     # each of the attributes of the LibraryItem instance.
 
+    print(f"{book.title}, {book.author}, {book.genre.name}")   
     
 
     # 3. Code a statement which creates an instance of the LibraryItem class with one or more invalid inputs.
     # Use your own unique valid values for the inputs to the class.
+
+    try:
+        invalid_book = LibraryItem(" ", "Author", Genre.NON_FICTION)
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == "__main__":
